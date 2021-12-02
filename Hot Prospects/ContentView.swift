2dc43ea2cfaf077f9cd39eb0f3998a0f38788374
Yash Poojary <yashspoojary245@gmail.com>
2021-12-02 15:53:25 +0530

    Initial Commit

---
+++ Hot Prospects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hot Prospects
+//
+//  Created by Yash Poojary on 02/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hot Prospects/Hot_ProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hot_ProspectsApp.swift
+//  Hot Prospects
+//
+//  Created by Yash Poojary on 02/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Hot_ProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
